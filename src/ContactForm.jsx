3e5219edd168ff102c0339bfbef322dab3e21511
Kapil Pagani <kapil.pagani@gmail.com>
2023-10-02 23:57:45 +0530

    Updated form

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -5,10 +5,10 @@ const ContactForm = () => {
   return (
     <>
     <Header />
-    <div className='container'> 
+    <div className='container' style={{border: '5px solid green'}}> 
     <h1 className='mt-5 text-center'>Name : Komal Jamnani</h1> 
     <h2 className='mt-3 text-center'>Email Address: KJAMNANI95@GMAIL.COM</h2>
-    <h2 className='mt-3 text-center'>University Name: The Maharaja Sayajirao University of Baroda</h2>
+    <h2 className='my-3 text-center'>University Name: The Maharaja Sayajirao University of Baroda</h2>
   </div>
   </>
   )
