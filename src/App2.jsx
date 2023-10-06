1f58f52d43ce4e129940d8b919d266a88a4c8425
Kapil Pagani <kapil.pagani@gmail.com>
2023-10-06 10:56:35 +0530

    Updated form

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-// import emailjs from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
 import Header from './Header';
 
 
